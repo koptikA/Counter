e2262414cb79a3bce92393e2894170d6aa76a89b
koptikA <koptik09051977@gmail.com>
2024-02-04 20:23:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Greeting } from './Greeting';
+import { Counter } from './Counter'
+
 
 function App() {
+  const title = 'Привет, мир!'
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeting name="Вася"  />
+      <Greeting name="Гоша"  />
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+
+
+export default App
